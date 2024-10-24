d1b99a20547f51156033e6d0f357b2008f7c295f
Alma Secerbegovic <alma.secerbegovic@fet.ba>
2024-10-24 14:31:25 +0200

    kod za veci broj primjer

--- lecture4/p4primjer2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ lecture4/p4primjer2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // kod za generiranje random broja
+        var r = Random.nextInt(10)
     }
 }
